49a982d5196dd43349244ac413ca7ff82f91cdae
violfedoseenko <viol.fedoseenko@yandex.ru>
2024-03-07 14:32:28 +0100

    fix url

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
         <div className="App">
             <Header/>
             <Routes>
-            <Route index element={<ProductList />}/>
+                <Route index element={<ProductList />}/>
                 <Route path={'form'} element={<Form />}/>
             </Routes>
     </div>
