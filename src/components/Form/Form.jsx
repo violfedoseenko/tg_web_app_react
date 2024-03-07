1e4bcb2382c6421f1cab2f1426e8473d9072f785
violfedoseenko <viol.fedoseenko@yandex.ru>
2024-03-07 14:46:04 +0100

    add submitting in form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -9,6 +9,22 @@ const Form = () => {
     const [subject, setSubject] = useState('physical');
     const {tg} = useTelegram();
 
+    const onSendData = useCallback(() => {
+        const data = {
+            country,
+            street,
+            subject
+        }
+        tg.sendData(JSON.stringify(data));
+    }, [country, street, subject])
+
+    useEffect(() => {
+        tg.onEvent('mainButtonClicked', onSendData)
+        return () => {
+            tg.offEvent('mainButtonClicked', onSendData)
+        }
+    }, [onSendData])
+
     useEffect(() => {
         tg.MainButton.setParams({
             text: 'Отправить данные'
